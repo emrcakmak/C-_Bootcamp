9e444e8d23dccd2de2dfc00a34ae3cde8838aa03
ckemr <emrecakmak123q@gmail.com>
2024-02-07 00:18:12 +0300

    MaskeTakip

---
+++ MaskeTakip/Workaround/WorkAround.cs
@@ -0,0 +1,38 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Workaround
+{
+     class WorkAround
+    {
+
+        static void Main(string[] args)
+        {
+            Person person1=new Person();
+            person1.FirstName = "Emre";
+            person1.LasttName = "çAKMAK";
+            person1.DateOfBirthYear =2000;
+            person1.NationalIdentity = 123;
+
+
+            PttManager pttmanager=new PttManager(new PersonManager());
+            pttmanager.GiveMask(person1);
+
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+
+    }
+}
